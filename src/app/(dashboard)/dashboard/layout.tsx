209b55f66799229ade16a36c25a5e9173f1249da
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-04-11 13:38:45 +0530

    layout finsished

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,6 +1,8 @@
+import SignOutButton from "@/components/SignOutButton";
 import { Icon, Icons } from "@/components/icons";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { FC, ReactNode } from "react";
@@ -65,6 +67,29 @@ const Layout = async ({ children }: LayoutProps) => {
                   );
                 })}
               </ul>
+            </li>
+            <li className='-mx-6 mt-auto flex items-center'>
+              <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
+                <div className='relative h-8 w-8 bg-gray-50'>
+                  <Image
+                    fill
+                    referrerPolicy='no-referrer'
+                    className='rounded-full'
+                    src={session.user.image || ''}
+                    alt='Your profile picture'
+                  />
+                </div>
+
+                <span className='sr-only'>Your profile</span>
+                <div className='flex flex-col'>
+                  <span aria-hidden='true'>{session.user.name}</span>
+                  <span className='text-xs text-zinc-400' aria-hidden='true'>
+                    {session.user.email}
+                  </span>
+                </div>
+              </div>
+
+              <SignOutButton className='h-full aspect-square' />
             </li>
           </ul>
         </nav>
