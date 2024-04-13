e827366df49ac0f64508f20a37da63de1654a248
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-04-13 20:07:45 +0530

    dashboard completed

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,20 +1,102 @@
-import { db } from "@/lib/db";
-import { FC } from "react";
-import { getServerSession } from "next-auth";
+import { getFriendsByUserId } from "@/helpers/get-friends-by-user-id";
+import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
-interface Props {}
+import { chatHrefConstructor } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
-const page: FC<Props> = async () => {
+const page = async ({}) => {
   const session = await getServerSession(authOptions);
-  console.log("session = ", session);
+  if (!session) notFound();
 
-  return  <div className="max-w-xs rounded overflow-hidden shadow-lg bg-white">
-  <img className="w-32 h-32 rounded-full mx-auto mt-4" src={session?.user?.image || ""} alt="Profile" />
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">{session?.user.name}</div>
-    <p className="text-gray-700 text-base">{session?.user.email}</p>
-  </div>
-</div>
+  const friends = await getFriendsByUserId(session.user.id);
+
+  const friendsWithLastMessage = await Promise.all(
+    friends.map(async (friend) => {
+      const [lastMessageRaw] = (await fetchRedis(
+        "zrange",
+        `chat:${chatHrefConstructor(session.user.id, friend.id)}:messages`,
+        -1,
+        -1
+      )) as string[];
+
+      console.log(lastMessageRaw);
+
+      const lastMessage = lastMessageRaw === undefined ? { text: "No Chats yet" } : JSON.parse(lastMessageRaw);
+      console.log(lastMessage);
+
+      return {
+        ...friend,
+        lastMessage,
+      };
+    })
+  );
+
+  return (
+    <div className="container py-12">
+      <div className="max-w-xs rounded overflow-hidden shadow-lg mb-10 bg-white">
+        <img
+          className="w-32 h-32 rounded-full mx-auto mt-4"
+          src={session?.user?.image || ""}
+          alt="Profile"
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{session?.user.name}</div>
+          <p className="text-gray-700 text-base">{session?.user.email}</p>
+        </div>
+      </div>
+      <h1 className="font-bold text-5xl mb-8">Recent chats</h1>
+      {friendsWithLastMessage.length === 0 ? (
+        <p className="text-sm text-zinc-500">Nothing to show here...</p>
+      ) : (
+        friendsWithLastMessage.map((friend) => (
+          <div
+            key={friend.id}
+            className="relative bg-zinc-50 border border-zinc-200 p-3 rounded-md"
+          >
+            <div className="absolute right-4 inset-y-0 flex items-center">
+              <ChevronRight className="h-7 w-7 text-zinc-400" />
+            </div>
+
+            <Link
+              href={`/dashboard/chat/${chatHrefConstructor(
+                session.user.id,
+                friend.id
+              )}`}
+              className="relative sm:flex"
+            >
+              <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
+                <div className="relative h-6 w-6">
+                  <Image
+                    referrerPolicy="no-referrer"
+                    className="rounded-full"
+                    alt={`${friend.name} profile picture`}
+                    src={friend.image}
+                    fill
+                  />
+                </div>
+              </div>
+
+              <div>
+                <h4 className="text-lg font-semibold">{friend.name}</h4>
+                <p className="mt-1 max-w-md">
+                  <span className="text-zinc-400">
+                    {friend.lastMessage.senderId === session.user.id
+                      ? "You: "
+                      : ""}
+                  </span>
+                  {friend.lastMessage.text}
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default page;
